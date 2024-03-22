b22dfabb0d4226da689e6481899e9d527c446a93
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-03-22 18:44:00 -0300

    feat: add database support to store messages

--- main.go
+++ main.go
@@ -2,20 +2,87 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 	"github.com/waldirborbajr/bombot/internal/config"
+	"github.com/waldirborbajr/bombot/internal/database"
 )
 
-// Send any text message to the bot after the bot has been started
+var (
+	db   *database.Database
+	err  error
+	help string
+)
+
+// handler is a default handler that simply sends a message to the chat.
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	message := ""
+	r := ""
+
+	if update.Message == nil ||
+		update.Message.Text == "" ||
+		update.Message.From == nil {
+		return
+	}
+
+	chatID := update.Message.Chat.ID
+	message = update.Message.Text
+
+	err := db.AddMessage("query", message, r)
+	if err != nil {
+		log.Printf("Error adding message to database: %v", err)
+	}
+
+	if strings.Contains(message[0:1], "/") {
+		switch update.Message.Text {
+		case "/start":
+			message = "Welcome to the BomBot!"
+		}
+	}
+
+	fmt.Println("ID: ", chatID)
+
+	if !update.Message.From.IsBot {
+		log.Println("From Bot: ")
+	}
+
+	// b.SendMessage(ctx, &bot.SendMessageParams{
+	// 	ChatID: chatID,
+	// 	Text:   message,
+	// })
+}
 
 func main() {
+	// initialize log config
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	db, err = database.New()
+	if err != nil {
+		log.Fatalf("Error creating database: %v", err)
+	}
+
+	helpAux, err := os.ReadFile("help.md")
+	if err != nil {
+		if !os.IsNotExist(err) {
+			log.Fatalf("Error reading help.md: %v", err)
+		}
+		log.Println("help.md not found")
+	}
+	help = string(helpAux)
+
+	// --------------------------------
+	/// Bot Initialization
+	// --------------------------------
+
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+
 	defer cancel()
 
 	opts := []bot.Option{
@@ -23,38 +90,60 @@ func main() {
 		bot.WithDebug(),
 	}
 
-	b, err := bot.New(config.BotToken, opts...)
-	if nil != err {
-		// panics for the sake of simplicity.
-		// you should handle this error properly in your code.
-		panic(err)
-	}
-
-	b.SetWebhook(ctx, &bot.SetWebhookParams{
-		URL: config.BotUrl,
-	})
-
-	go func() {
-		http.ListenAndServe(":9090", b.WebhookHandler())
-	}()
-
-	// Use StartWebhook instead of Start
-	b.StartWebhook(ctx)
-
-	// call methods.DeleteWebhook if needed
-}
-
-func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
-	if update.Message == nil ||
-		update.Message.Text == "" ||
-		update.Message.From == nil {
+	telegramBotToken := config.BotToken
+	if telegramBotToken == "" {
+		log.Println("TELEGRAM_BOT_TOKEN environment variable is not set")
 		return
 	}
 
-	log.Printf("From: %v\nText: %v\n", update.Message.From, update.Message.Text)
+	b, err := bot.New(telegramBotToken, opts...)
+	if nil != err {
+		log.Fatalf("Error creating bot: %v", err)
+	}
 
-	b.SendMessage(ctx, &bot.SendMessageParams{
-		ChatID: update.Message.Chat.ID,
-		Text:   update.Message.Text,
+	webHookUrl := config.BotUrl
+	if webHookUrl == "" {
+		log.Println("webHook URL environment variable is not set")
+		return
+	}
+
+	_, err = b.SetWebhook(ctx, &bot.SetWebhookParams{
+		URL: webHookUrl,
 	})
+	if err != nil {
+		log.Printf("Error on SetWebhook: %v", err)
+		return
+	}
+
+	// Use StartWebhook instead of Start
+
+	go b.StartWebhook(ctx)
+
+	b.SetMyCommands(ctx, &bot.SetMyCommandsParams{
+		Commands: []models.BotCommand{
+			{
+				Command:     "explain",
+				Description: "Explain the following text",
+			},
+			{
+				Command:     "translate",
+				Description: "Translate from any language to any other language",
+			},
+			{
+				Command:     "image",
+				Description: "Generate an image from text",
+			},
+		},
+	})
+
+	http.ListenAndServe(":2000", b.WebhookHandler())
+
+	log.Println("BomBot started")
+
+	// call methods.DeleteWebhook if needed
+	_, err = b.DeleteWebhook(ctx, &bot.DeleteWebhookParams{DropPendingUpdates: true})
+	if err != nil {
+		log.Printf("Error on DeleteWebhook: %v", err)
+		return
+	}
 }
